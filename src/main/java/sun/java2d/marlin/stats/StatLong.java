c33449430d1b5710fc984cbc3af1c4bf3ad24013
bourgesl <bourges.laurent@gmail.com>
2015-03-13 17:56:00 +0100

    fixed copyright

--- src/main/java/sun/java2d/marlin/stats/StatLong.java
+++ src/main/java/sun/java2d/marlin/stats/StatLong.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -94,3 +94,4 @@ public class StatLong {
         return ((long) (1e3d * value)) / 1e3d;
     }
 }
+
