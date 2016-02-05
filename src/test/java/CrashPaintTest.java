726537858bc23947a73013ddd49dba8e60905b9a
bourgesl <bourges.laurent@gmail.com>
2016-02-06 00:05:59 +0100

    fixed reentrance in AAShapePipe (using TL + CLQ) + improved CrashPaintTest to check few pixel values (ensure proper rendering)

--- src/test/java/CrashPaintTest.java
+++ src/test/java/CrashPaintTest.java
@@ -21,9 +21,9 @@
  * questions.
  */
 
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Paint;
 import java.awt.PaintContext;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
@@ -33,6 +33,7 @@ import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
+import java.awt.image.Raster;
 import java.io.File;
 import java.io.IOException;
 import java.util.Locale;
@@ -117,18 +118,19 @@ public class CrashPaintTest {
             final Ellipse2D.Double ellipse
                 = new Ellipse2D.Double(0, 0, width, height);
 
-            final long start = System.nanoTime();
+            final Paint paint = new CustomPaint(100);
 
-            g2d.setPaint(new CustomPaint(100));
-            g2d.fill(ellipse);
+            for (int i = 0; i < 20; i++) {
+                final long start = System.nanoTime();
+                g2d.setPaint(paint);
+                g2d.fill(ellipse);
 
-            g2d.setColor(Color.GREEN);
-            g2d.setStroke(new BasicStroke(1f));
-            g2d.draw(ellipse);
+                g2d.setColor(Color.GREEN);
+                g2d.draw(ellipse);
 
-            final long time = System.nanoTime() - start;
-
-            System.out.println("paint: duration= " + (1e-6 * time) + " ms.");
+                final long time = System.nanoTime() - start;
+                System.out.println("paint: duration= " + (1e-6 * time) + " ms.");
+            }
 
             if (SAVE_IMAGE) {
                 try {
@@ -141,8 +143,34 @@ public class CrashPaintTest {
                     ex.printStackTrace();
                 }
             }
+
+            // Check image on few pixels:
+            final Raster raster = image.getData();
+
+            // 170, 175 = blue
+            checkPixel(raster, 170, 175, Color.BLUE.getRGB());
+            // 50, 50 = blue
+            checkPixel(raster, 50, 50, Color.BLUE.getRGB());
+
+            // 190, 110 = pink
+            checkPixel(raster, 190, 110, Color.PINK.getRGB());
+            // 280, 210 = pink
+            checkPixel(raster, 280, 210, Color.PINK.getRGB());
+
         } finally {
             g2d.dispose();
+        }
+    }
+
+    private static void checkPixel(final Raster raster,
+                                   final int x, final int y,
+                                   final int expected) {
+
+        final int[] rgb = (int[]) raster.getDataElements(x, y, null);
+
+        if (rgb[0] != expected) {
+            throw new IllegalStateException("bad pixel at (" + x + ", " + y
+                + ") = " + rgb[0] + " expected: " + expected);
         }
     }
 
