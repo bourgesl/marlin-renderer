8beaf02154c9fa4dc1c76f9d4d61e8a55e6366e2
bourgesl <bourges.laurent@gmail.com>
2019-09-06 22:28:18 +0200

    cleanup FlipBitTest.java

--- src/test/java/FlipBitTest.java
+++ src/test/java/FlipBitTest.java
@@ -53,19 +53,6 @@ public class FlipBitTest {
         // First display which renderer is tested:
         // JDK9 only:
         System.setProperty("sun.java2d.renderer.verbose", "true");
-        System.out.println("Testing renderer: ");
-        // Other JDK:
-        String renderer = "undefined";
-        try {
-            renderer = sun.java2d.pipe.RenderingEngine.getInstance().getClass().getName();
-            System.out.println(renderer);
-        } catch (Throwable th) {
-            // may fail with JDK9 jigsaw (jake)
-            if (false) {
-                System.err.println("Unable to get RenderingEngine.getInstance()");
-                th.printStackTrace();
-            }
-        }
 
         System.out.println("FlipBitTest: size = " + size);
 
